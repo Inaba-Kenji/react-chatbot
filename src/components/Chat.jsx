74256e4efd5258b7be1999392d0752334d8d6c1a
Inaba Kenji <k.inaba040332@gmail.com>
2021-01-02 20:52:18 +0900

    finish: react-chatbot6章　

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import NoProfile from '../assets/img/no-profile.png'
+import Torahack from '../assets/img/torahack.png'
 
 const Chat = (props) => {
   // 真偽値が代入される
@@ -10,9 +12,13 @@ const Chat = (props) => {
   const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
 
   return(
-    <ListItem>
-        <ListItemAvatar className={classes}>
-          <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+    <ListItem className={classes}>
+        <ListItemAvatar>
+          {isQuestion ? (
+            <Avatar alt="icon" src={Torahack} />
+          ) : (
+            <Avatar alt="icon" src={NoProfile} />
+          )}
         </ListItemAvatar>
   <div className="p-chat__bubble">{props.text}</div>
       </ListItem>
