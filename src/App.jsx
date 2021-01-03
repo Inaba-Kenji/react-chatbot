780b5fa95bd096cc383c8f2ecaccb9e0a52b6d8f
Inaba Kenji <k.inaba040332@gmail.com>
2021-01-03 11:55:47 +0900

    finish: react-chatbot8章

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,14 @@ export default class App extends React.Component {
   selectAnswer = (selectAnswer, nextQuestionId) => {
     switch(true) {
       case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId)
+         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
          break;
+      case (/^https:*/.test(nextQuestionId)):
+        const a = document.createElement('a');
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
+        break;
       default:
         const chats = this.state.chats;
         chats.push({
@@ -48,7 +54,7 @@ export default class App extends React.Component {
           chats:　chats
         })
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout( () => this.displayNextQuestion(nextQuestionId), 1000);
          break;
     }
   }
@@ -56,6 +62,13 @@ export default class App extends React.Component {
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area');
+    if(scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight;
+    }
   }
 
 
